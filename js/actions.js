edb9ae5daf84461cef9ef31ab82bbf4775db6368
marbaque <mario.badilla@gmail.com>
2017-11-24 21:52:05 -0600

    cap1

---
+++ js/actions.js
@@ -0,0 +1,9 @@
+$('.accordion').on('click', '.panel-title', function(e){ // When clicked
+  e.preventDefault();                    // Prevent default action of button
+  $(this)                                // Get the element the user clicked on
+    .next('.accordion-panel')            // Select following panel 
+    .not(':animated')                    // If it is not currently animating
+    .slideToggle();                      // Use slide toggle to show or hide it
+    
+    
+});
